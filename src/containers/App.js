b4b27cbaad59bcb287a8ffdce76fa8ad2b4131ed
MAwais <mawais54013@outlook.com>
2019-05-14 21:55:31 -0700

    redux practice

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../Components/CardList';
 import SearchBox from '../Components/SearchBox';
 import Scroll from '../Components/Scroll';
 import ErrorBoundry from '../Components/ErrorBoundry'
 import '../containers/App.css'
+
+import { setSearchField } from '../actions';
+
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
 
 class App extends Component {
     constructor() {
@@ -19,13 +34,10 @@ class App extends Component {
             .then(response => response.json())
             .then(users => { this.setState({ robots: users }) });
     }
-
-    onSearchChange = (event) => {
-        this.setState({ searchField: event.target.value })
-    }
-
+    
     render() {
-        const { robots, searchField } = this.state;
+        const { robots } = this.state;
+        const { searchField, onSearchChange } = this.props;
         const filter = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchField.toLowerCase());
         });
@@ -34,7 +46,7 @@ class App extends Component {
             (
                 <div className='tc'>
                     <h1 className='f2'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
+                    <SearchBox searchChange={onSearchChange} />
                     <Scroll>
                         <ErrorBoundry>
                             <CardList robots={filter} />
@@ -45,4 +57,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
